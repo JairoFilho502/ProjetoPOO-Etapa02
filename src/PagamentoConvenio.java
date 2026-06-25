356a8584236a42766e75777f11c69d861fd6f83c
João Victor Serrano Costa <jvffserrano@gmail.com>
2026-06-25 20:59:06 -0300

    criei uma outra classe pagamentoconvenio p aplicar cobertura percentual

---
+++ src/PagamentoConvenio.java
@@ -0,0 +1,17 @@
+public class PagamentoConvenio extends Pagamento {
+    private String especialidadeConsulta;
+    private double taxaCoberturaTemporaria; 
+
+    public PagamentoConvenio(int indiceConsulta, double valorBase, String especialidadeConsulta, double taxaCoberturaTemporaria) {
+        super(indiceConsulta, valorbase); 
+        this.especialidadeConsulta = especialidadeConsulta;
+        this.taxaCoberturaTemporaria = taxaCoberturaTemporaria;
+    }
+    @Override
+    public double calcularValorFinal() { 
+        double desconto = getValorBase() * taxaCoberturaTemporaria;
+        return getValorBase() - desconto;
+    }
+}
+
+ 
