38fa1f3b181298783349671d0d8d1e2a11d78f02
Esdras Virgolino <rbrdtbd4t5@privaterelay.appleid.com>
2026-06-28 20:46:27 -0300

    Delete src/Agendavel.java

--- src/Agendavel.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-// qualquer coisa que pode ser agendada precisa implementar esses três métodos
-public interface Agendavel {
-    void agendar();   // marca como agendado
-    void cancelar();  // cancela o agendamento
-    void remarcar();  // muda pra outro horário
-}
