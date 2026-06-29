356b6b97fc1a92c3e4b55000e2e16e132822488d
José Guilherme Ferreira Alves <jose.alves007@cs.unipe.edu.br>
2026-06-28 23:49:48 -0300

    Delete OperacaoInvalidaException.java

--- OperacaoInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-// convenio do paciente nao cobre o tipo de consulta solicitada
-public class ConvenioNaoCobreException extends Exception {
-    public ConvenioNaoCobreException(String mensagem) { super(mensagem); }
-    public ConvenioNaoCobreException(String mensagem, Throwable causa) { super(mensagem, causa); }
-}
