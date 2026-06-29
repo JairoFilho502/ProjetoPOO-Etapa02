ff5bc9c94a12dde68efdc1b8d85b80d1944c539f
JairoFilho502 <jairogeorge.adv@gmail.com>
2026-06-28 23:49:21 -0300

    Delete Pagamento.java

--- Pagamento.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-// runtimeexception  erros de pagamento sem obrigar o try-catch
-public class PagamentoInvalidoException extends RuntimeException {
-    public PagamentoInvalidoException(String mensagem) { super(mensagem); }
-    public PagamentoInvalidoException(String mensagem, Throwable causa) { super(mensagem, causa); }
-}
