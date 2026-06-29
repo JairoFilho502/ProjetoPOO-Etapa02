f1d9a4df3318fe7e78e7e03a707ef8b01229de82
JairoFilho502 <jairogeorge.adv@gmail.com>
2026-06-28 23:48:13 -0300

    Delete Consulta.java

--- Consulta.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-// coisas agendaveis implementam esses tres metodos
-public interface Agendavel {
-    void agendar();   // marca como agendado
-    void cancelar();  // cancela o agendamento
-    void remarcar();  // muda pra outro horario
-}
