993c49c3f086519ec2783dd67052d974fccf7c99
JairoFilho502 <jairogeorge.adv@gmail.com>
2026-06-28 23:49:52 -0300

    Delete PacienteInativoException.java

--- PacienteInativoException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-// lancada quando o horario que o paciente quer ja esta ocupado
-public class HorarioIndisponivelException extends Exception {
-    public HorarioIndisponivelException(String mensagem) { super(mensagem); }
-    public HorarioIndisponivelException(String mensagem, Throwable causa) { super(mensagem, causa); }
-}
